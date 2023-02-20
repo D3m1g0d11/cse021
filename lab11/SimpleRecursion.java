d80ca354d0f7f5468be2a4a2d71eb3e0c724f8a8
John Biton <80254416+D3m1g0d11@users.noreply.github.com>
2023-02-20 13:39:39 -0800

    Add files via upload

---
+++ lab11/SimpleRecursion.java
@@ -0,0 +1,73 @@
+package recursions;
+
+/*  This recursive method is a slightly augmented 
+	version of the program in the last step: it only makes a recursive
+	call under certain conditions. There are three calls that print to the 
+	console. See if you can predict what will get printed and in what order.
+ */
+public class SimpleRecursion {
+
+	// version 1
+	public static void recur(int k) {
+		System.out.println("Entering recur with k = " + k);
+		if(k <= 3) {
+			System.out.println("Recursing with k = " + k);
+			recur(k + 1);
+		}
+		System.out.println("Leaving recur with k = " + k);
+	}
+
+	
+	
+	// version 2
+	public static void recurB(int k) {
+		System.out.println("count = " + k);
+		if(k != 5) {
+			recur(k + 1);
+		}
+	}
+	
+	
+	// version 3
+	public static void recurC(int k) {
+		if(k == 5) {
+			System.out.println("count = " + k);
+			recur(k + 1);
+		}
+	}
+	
+	
+	// version 4
+	public static void recurD(int k) {
+		if(k < 5) {
+			System.out.println("count = " + k);
+			recur(k + 1);
+		}
+	}
+	
+	// version 5
+	public static void recurE(int k) {
+		System.out.println("count = " + k);
+		if(k == 5) {
+			recur(k + 1);
+		}
+	}
+	
+	// version 6
+	public static void recurF(int k) {
+		if(k != 5) {
+			System.out.println("count = " + k);
+			recur(k + 1);
+		}
+	}
+	
+	public static void main(String[] args) {
+		recur(0);
+		recurB(1);
+		recurC(2);
+		recurD(3);
+		recurE(4);
+		recurF(5);
+		
+	}
+}
